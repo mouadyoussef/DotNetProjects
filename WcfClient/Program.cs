8ca8f9246c814869ce01e836494d695ed97b71ea
Mouad Youssef <imouad.you@gmail.com>
2019-03-08 19:31:36 +0100

    Adding WCF service and client.

---
+++ WcfClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfClient.WcfServiceProducts;
+
+namespace WcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceProducts service = new ServiceProductsClient();
+            Product product = service.Get(1);
+
+            Console.WriteLine($"Id = {product.Id}, Name = {product.Name}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ WcfService/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Web;
+
+namespace WcfServiceApp
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public double Price { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(int id, string name, double price, string description, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Description = description;
+            Quantity = quantity;
+        }
+        
+        public override string ToString()
+        {
+            return $"ID = {Id}, Name = {Name}, Price = {Price}";
+        }
+    }
+}
